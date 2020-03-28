ce99e234680091d5ecd89a67a7f26d1c07cc53da
Kaique Ryan <kaique.github@gmail.com>
2020-03-28 18:09:18 -0300

    Update routes file

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const connection = require('./database/connection');
+const crypto = require('crypto');
 const routes = express.Router();
 
 routes.post('/ongs', (req, res) => {
 	const {name, email, whatsapp, city, uf} = req.body;
-
-
+	
     return res.json(data);
 });
 
