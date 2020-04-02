12d1706a2dc7ed28e0d4e77095e2bca2d007e6e2
Kaique Ryan <kaique.github@gmail.com>
2020-04-02 19:07:10 -0300

    Update file routes.js for add new routes incidents and ongs

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 const OngController = require('./controllers/OngController.js');
+const IncidentsController = require('./controllers/incidentsController.js');
 
-routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs/create', OngController.create);
+routes.post('/ongs/delete', OngController.delete);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents/create', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
 
 module.exports = routes;
