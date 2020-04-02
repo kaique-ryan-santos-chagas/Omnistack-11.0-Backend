dab49bf9d44690fbedbca50e20d298de0ba2a223
Kaique Ryan <kaique.github@gmail.com>
2020-04-02 19:06:21 -0300

    Add controller for api routes

--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -3,6 +3,11 @@ const connection = require('../database/connection');
 
 
 module.exports = {
+	index: async (req, res) => {
+		const ongs = await connection('ongs').select('*');
+		return res.json(ongs);
+	},
+	
 	create: async (req, res) => {
 		const {name, email, whatsapp, city, uf} = req.body;
 		const id = crypto.randomBytes(4).toString('HEX');
@@ -15,6 +20,14 @@ module.exports = {
 			uf
 		});
 		return res.json({ id });
+	},
+
+	// Revisar o código (não está funcionando)
+	delete: async (req, res) => {
+		const idOng = req.body;
+		const ongDel = await connection('ongs').where('id', idOng).del();
+		return res.send('Account deleted');
+		
 	}
 
-}
+};
---
+++ src/controllers/incidentsController.js
@@ -0,0 +1,33 @@
+const connection = require('../database/connection');
+
+module.exports = {
+	index: async (req, res) => {
+		const incidents = await connection('incidents').select('*');
+		return res.json(incidents);
+	},
+
+	create: async (req, res) => {
+		const {title, description, value} = req.body;
+		const ong_id = req.headers.authorization;
+		const [id] = await connection('incidents').insert({
+			title,
+			description,
+			value,
+			ong_id
+		});
+		return res.json({ id });
+	},
+
+	delete: async (req, res) => {
+		const { id } = req.params;
+		const ong_id = req.headers.authorization;
+		const incident = await connection('incidents').where('id', id).select('ong_id').first();
+		if (incident.ong_id != ong_id) {
+			return res.status(401).json({ error: 'Operation is not permitted.' });
+		}
+			
+		await connection('incidents').where('id', id).delete();
+		return res.status(204).send();
+		
+	} 
+};
