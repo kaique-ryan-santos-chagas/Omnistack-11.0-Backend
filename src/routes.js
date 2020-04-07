c391767add0d360e3ea70868e6e4cd2370828116
Kaique Ryan <kaique.github@gmail.com>
2020-04-07 14:35:36 -0300

    New routes added in application

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,18 @@ const express = require('express');
 const routes = express.Router();
 const OngController = require('./controllers/OngController.js');
 const IncidentsController = require('./controllers/incidentsController.js');
+const ProfileController = require('./controllers/profileController.js');
+const SessionController = require('./controllers/sessionController.js');
+
+routes.post('/session', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs/create', OngController.create);
-routes.post('/ongs/delete', OngController.delete);
+routes.delete('/ongs/:ongName', OngController.delete);
+routes.get('/ongs/search', OngController.searchOng);
+
+routes.get('/profile', ProfileController.index);
+routes.post('/profile', ProfileController.searchIncident);
 
 routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents/create', IncidentsController.create);
